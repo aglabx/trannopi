f681f5e127ac61bd62e06388278db980a168298e
zilov <zilov@scamt-itmo.ru>
2021-03-12 16:12:16 +0000

    add v2trim, rmdub, trinity

--- rules/rmdup.smk
+++ rules/rmdup.smk
@@ -1,20 +1,19 @@
-rule clean up optical duplicates:
+rule rmdup:
     input:
-        v2trim_out_forward = config["v2trim_out_file1"] ,
-        v2trim_out_reverse = config["v2trim_out_file2"],
+        v2trim_out_forward = rules.trimming.output.v2trim_out_forward,
+        v2trim_out_reverse = rules.trimming.output.v2trim_out_reverse,
     conda:
-        "/home/nsorokina/snakemake/rmdub/envs/rmdub.yaml"
+        envs.rmdup
     threads: 20
     output:
-        rmdup_out_forward = config["rmdup_out_file1"] ,
+        rmdup_out_forward = config["rmdup_out_file1"],
         rmdup_out_reverse = config["rmdup_out_file2"],
         rmdup_out_statistics = config["rmdup_out_statistics"] ,
     params:
-        rmdup_dir = config["rmdup_dir"] 
+        rmdup_dir = directory(config["rmdup_dir"]) 
     shell:
         """rmdup -1 {input.v2trim_out_forward} \
                  -2 {input.v2trim_out_reverse} \
                  -o {params.rmdup_dir} \
-                 -t {threads} \
-                 -u"""
+                 -t {threads} """
 
--- transpipe.py
+++ transpipe.py
@@ -1,1 +1,55 @@
-print("Hello, GitHub")
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#@created: <data>
+#@author: <name>
+#@contact: <email>
+
+import sys
+import argparse
+import os
+import os.path
+    
+
+
+def main():
+    ''' Function description.
+    '''
+        
+    if debug:
+        snake_debug = "-n"
+    else:
+        snake_debug = ""
+
+
+    #Config-maker
+    command = f"python ./config-maker.py -1 {forward_read} -2 {reverse_read} -o {outdir}"
+    print(command)
+    os.system(command)
+    #Snakemake
+    command = f"snakemake --cores {threads} --use-conda {snake_debug}"
+    print(command)
+    os.system(command)
+        
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Pannopi - a tool for prokariotic genome assembly and annotation.')
+    parser.add_argument('-1','--forward_read', help="path to forward read")
+    parser.add_argument('-2','--reverse_read', help='path to reverse read')
+    parser.add_argument('-o','--outdir', help='output directory', default=False)
+    parser.add_argument('-t','--threads', help='number of threads [default == 8]', default = "8")
+    parser.add_argument('-d','--debug', help='debug mode', action='store_true')
+    args = vars(parser.parse_args())
+
+    forward_read = os.path.abspath(args["forward_read"])
+    reverse_read = os.path.abspath(args["reverse_read"])
+    outdir = args["outdir"]
+    if not outdir:
+        outdir = os.path.dirname(forward_read)
+    else:
+        outdir = os.path.abspath(outdir)
+    threads = args["threads"]
+    debug = args["debug"]
+
+    main()
